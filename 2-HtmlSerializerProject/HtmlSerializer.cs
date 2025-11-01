e8702cbaf2696a1452140ca85d95e1e40a6d72ee
yehudit <y750563@gmail.com>
2025-11-02 01:05:32 +0200

    הוספת קבצי פרויקט חשובים בלבד

---
+++ 2-HtmlSerializerProject/HtmlSerializer.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace _2_HtmlSerializerProject
+{
+    internal class HtmlSerializer
+    {
+        private readonly HtmlParser _parser;
+
+        public HtmlSerializer()
+        {
+            _parser = new HtmlParser();
+        }
+
+        // פונקציה שמביאה HTML מכתובת אינטרנט
+        public async Task<string> Load(string url)
+        {
+            using HttpClient client = new HttpClient();
+            var response = await client.GetAsync(url);
+            var html = await response.Content.ReadAsStringAsync();
+            return html;
+        }
+        public HtmlElement Serialize(string html)
+        {
+            var cleanHtml = new Regex(@"\s*").Replace(html, "");
+            var htmlLines = new Regex(@"(<.+?>)").Split(cleanHtml).Where(s => s.Length > 0);
+            HtmlElement root = _parser.BuildTree(htmlLines);
+            return root;
+        }
+    }
+
+}
--- 2-HtmlSerializerProject/Program.cs
+++ 2-HtmlSerializerProject/Program.cs
@@ -1,16 +1,17 @@
 ﻿using _2_HtmlSerializerProject;
 using System.Text.RegularExpressions;
-var html = await Load("https://www.w3schools.com");
+
+HtmlSerializer serializer = new HtmlSerializer();
+
+var html = await serializer.Load("https://www.w3schools.com");
 //var html = await Load("https://forum.netfree.link:20907/category/1/%D7%94%D7%9B%D7%A8%D7%96%D7%95%D7%AA");
 
-var cleanHtml = new Regex(@"\s*").Replace(html, "");
-var htmlLines = new Regex(@"(<.+?>)").Split(cleanHtml).Where(s => s.Length > 0);
+HtmlElement root = serializer.Serialize(html);
 var htmlElement = @"<div id=""my-id"" class=""my-class-1 my-class-2"" width=""100%"">text</div>";
 var attributes = new Regex(@"([\s\S]+?)=(""[^""]*""|'[^']*'|\S+)").Matches(htmlElement);
-HtmlParser parser = new HtmlParser();
-HtmlElement root = parser.BuildTree(htmlLines);
-//string query = "h1"; // לדוגמה
-string query = "div"; // לדוגמה
+
+string query = "h1"; // לדוגמה
+//string query = "div"; // לדוגמה
 
 Selector selector = Selector.Parse(query);
 
@@ -24,11 +25,5 @@ foreach (var el in results)
 {
     Console.WriteLine($"Tag: {el.Name}, Id: {el.Id}, Classes: {string.Join(",", el.Classes)}");
 }
-static async Task<string> Load(string url)
-{
-    HttpClient client = new HttpClient();
-    var response = await client.GetAsync(url);
-    var html = await response.Content.ReadAsStringAsync();
-    return html;
-}
+
 
